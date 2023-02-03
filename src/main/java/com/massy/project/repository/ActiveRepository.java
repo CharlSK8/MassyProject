fd1cb0d30a3f8bd51f65acf79df1fb139e1f8fdb
CharlSK8 <charlinson2013@gmail.com>
2023-02-03 16:13:08 -0500

    Repository is added employee

--- src/main/java/com/massy/project/repository/ActiveRepository.java
+++ src/main/java/com/massy/project/repository/ActiveRepository.java
@@ -1,9 +1,9 @@
 package com.massy.project.repository;
 
-import com.massy.project.Empleado;
+import com.massy.project.Activo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Empleado, Integer> {
+public interface ActiveRepository extends JpaRepository<Activo, String> {
 }
